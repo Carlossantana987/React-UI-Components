94724246b68390708565d3bf7acdb034aa90b33a
Carlossantana987 <carlossantana987@outlook.com>
2019-05-29 01:27:57 -0400

    NumberButton component finished

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,7 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+import NumberButton from "../ButtonComponents/NumberButton";
+import ActionButton from "../ButtonComponents/ActionButton";
+
+export default CalcDisplay;
