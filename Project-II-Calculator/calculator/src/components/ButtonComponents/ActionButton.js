5c04b5088240782f8c620c03f2da1485861c6645
Carlossantana987 <carlossantana987@outlook.com>
2019-05-29 01:32:17 -0400

    ActionButton component finished

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from "react";
 import "./Button.css";
+
+const ActionButton = props => {
+  return (
+    <div className="actionbutton">
+      <button>
+        {" "}
+        className= {`basicButtonStyles ${props.buttonStyles}`}
+        {props.text}{" "}
+      </button>
+    </div>
+  );
+};
+
+export default ActionButton;
